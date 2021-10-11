cf1ab15c9e4865f085ad1dd056814d347350d4ae
Robyn Bursnell <robyn.bursnell@asda.co.uk>
2021-10-11 16:34:59 +0100

    initial changes for MSSQL

--- data/init.sql
+++ data/init.sql
@@ -1,9 +1,44 @@
-CREATE DATABASE devdb;
+CREATE DATABASE mediaPlayer;
 GO
-USE devdb;
+
+USE mediaPlayer;
+
+CREATE TABLE dbo.Songs (
+    songId INT PRIMARY KEY,
+    songName VARCHAR(255),
+    artist VARCHAR(255),
+    location VARCHAR(255),
+);
+
+CREATE TABLE dbo.Playlists (
+    playlistId INT PRIMARY KEY,
+    playlistName VARCHAR(255),
+);
+
+CREATE TABLE dbo.PlaylistSongs (
+    playlistId INT,
+    songId INT,
+    CONSTRAINT PK_PLAYLISTSONGS PRIMARY KEY NONCLUSTERED ([playlistId], [songId])
+);
 GO
-CREATE TABLE Stores (
-    Number varchar(5),
-    Name varchar(255)
+
+INSERT INTO dbo.Songs (
+    songId,
+    songName,
+    artist,
+    location,
+) VALUES (
+    1,
+	'FunHouse',
+	'P!nk',
+	'./assets/songs/Funhouse.mp3',
+);
+
+INSERT INTO dbo.Playlists (
+    playlistId,
+    playlistName,
+) VALUES (
+    1,
+	'Rock'
 );
 GO
